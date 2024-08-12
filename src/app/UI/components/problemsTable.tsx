5a7358878d1f5ffe63dcc2a456f6e0fb14bf836f
Lucas Cardozo <lucasivancardozo27@gmail.com>
2024-08-12 12:36:49 -0300

    mejora css de imagenes de problemas

--- src/app/UI/components/problemsTable.tsx
+++ src/app/UI/components/problemsTable.tsx
@@ -46,47 +46,61 @@ export default async function ProblemsTable({
       {tps.map(
         ({ id_tps, materias_tps_problemas, name, number, year }, index) => (
           <li key={index} className="relative">
-            <div className="flex items-center text-xl sticky top-0 z-20 bg-[--platinum] py-1">
+            <div className="flex items-center text-xl sticky top-0 z-20 bg-[--platinum] py-1 ">
               {numberIcons[number] ? numberIcons[number] : numberIcons[0]}
               <h2>{name}</h2>
             </div>
-            <ul className="flex flex-col gap-4 pl-3">
+            <ul className="flex flex-col gap-1 pl-3">
               {materias_tps_problemas.map(({ problemas, number }, index) => (
                 <li
                   key={index}
                   className="bg-[--white] p-2 text-base leading-5 drop-shadow-md flex flex-col gap-1"
                 >
-                  {problemas.response_plus ? (
-                    <div className="relative grid grid-cols-[4fr_3fr] grid-rows-[minmax(10rem,1fr)] bg-[#C8E0E4] p-1 rounded-md">
-                      <p className="whitespace-pre-wrap">{problemas.text}</p>
-                      <div className="relative h-full">
-                        <Image
-                          className="object-contain"
-                          src={`${problemas.response_plus}.${problemas.type_plus}`}
-                          fill={true}
-                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                          alt="Imagen"
-                          priority
-                        />
-                      </div>
+                  <p className="whitespace-pre-wrap bg-[#C8E0E4] p-1 rounded-md">
+                    <b className="bg-[#92C1C9] rounded-sm">{`Problema ${number}:`}</b>
+                    <br />
+                    {problemas.response_plus ? (
+                      <span
+                        className={`${
+                          problemas.text.length > 200
+                            ? 'grid-cols-[1fr] grid-rows-[1fr,min]'
+                            : 'grid-cols-[1fr] grid-rows-[min,1fr]'
+                        } relative grid bg-[#C8E0E4] p-1`}
+                      >
+                        <span className="whitespace-pre-wrap pb-1 pr-1">
+                          {problemas.text}
+                        </span>
+                        <span className="relative max-h-96 flex">
+                          <Image
+                            className="object-contain"
+                            src={`${problemas.response_plus}.${problemas.type_plus}`}
+                            width={500}
+                            height={500}
+                            alt="Imagen"
+                            priority
+                          />
+                        </span>
+                      </span>
+                    ) : (
+                      <span className="whitespace-pre-wrap">
+                        {problemas.text}
+                      </span>
+                    )}
+                  </p>
+                  {problemas.response ? (
+                    <div className="relative flex justify-center w-full">
+                      <Image
+                        className="object-contain"
+                        src={`${problemas.response}.${problemas.type}`}
+                        width={500}
+                        height={500}
+                        alt="Imagen"
+                        priority
+                      />
                     </div>
                   ) : (
-                    <p className="whitespace-pre-wrap bg-[#C8E0E4] p-1 rounded-md">
-                      <b>{`Problema ${number}:`}</b>
-                      <br />
-                      {problemas.text}
-                    </p>
+                    ''
                   )}
-                  <div className="relative w-full aspect-video">
-                    <Image
-                      className="object-contain"
-                      src={`${problemas.response}.${problemas.type}`}
-                      fill={true}
-                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                      alt="Imagen"
-                      priority
-                    />
-                  </div>
                 </li>
               ))}
             </ul>
