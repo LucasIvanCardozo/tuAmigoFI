cbccff182812c254d7ea0a91713665c17440d3c3
Lucas Cardozo <lucasivancardozo27@gmail.com>
2024-08-12 02:56:13 -0300

    mejor manejo de db prisma

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,7 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
+import prisma from './db';
 // fetching de materias
 export async function fetchCourses({
   search,
