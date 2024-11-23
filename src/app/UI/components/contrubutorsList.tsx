fed51b3dd8664bb53cde5d4dcfb9c4da6f2adf4e
Lucas Cardozo <lucasivancardozo27@gmail.com>
2024-11-23 16:20:39 -0300

    Cambio cache de contrubuyentes

--- src/app/UI/components/contrubutorsList.tsx
+++ src/app/UI/components/contrubutorsList.tsx
@@ -25,18 +25,17 @@ export default function ContributorsList() {
   useEffect(() => {
     const fetchUsers = async () => {
       const users = await fetchContributors();
-      const prepareUsers = users
-        .map((user) => ({
-          ...user,
-          score:
-            user._count.links * 1 +
-            user._count.midterms * 5 +
-            user._count.tps * 6 +
-            user._count.tps_reactions * 1 +
-            user._count.tps_responses * 3,
-        }))
-        .filter((user) => user.score > 0)
-        .sort((a, b) => b.score - a.score);
+      const prepareUsers = users.map((user) => ({
+        ...user,
+        score:
+          user._count.links * 1 +
+          user._count.midterms * 5 +
+          user._count.tps * 6 +
+          user._count.tps_reactions * 1 +
+          user._count.tps_responses * 3,
+      }));
+      // .filter((user) => user.score > 0)
+      // .sort((a, b) => b.score - a.score);
       console.log(prepareUsers);
       setContributors(prepareUsers);
     };
