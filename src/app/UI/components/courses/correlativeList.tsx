4abe2bb880c24f8f6bf2b8837081acbf883344fd
Lucas Cardozo <lucasivancardozo27@gmail.com>
2024-11-28 15:04:22 -0300

    cambio de button por link

--- src/app/UI/components/courses/correlativeList.tsx
+++ src/app/UI/components/courses/correlativeList.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+// import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function CorrelativeList({
   index,
@@ -8,28 +9,31 @@ export default function CorrelativeList({
   index: number;
   name: string;
 }) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const { replace } = useRouter();
 
-  const handleClick = (search: string) => {
-    const normalizedSearch = search
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '');
-    const params = new URLSearchParams(searchParams);
-    params.set('search', normalizedSearch);
-    replace(`${pathname}?${params.toString()}`);
-  };
+  // const handleClick = (search: string) => {
+  //   const normalizedSearch = search
+  //     .normalize('NFD')
+  //     .replace(/[\u0300-\u036f]/g, '');
+  //   const params = new URLSearchParams(searchParams);
+  //   params.set('search', normalizedSearch);
+  //   replace(`${pathname}?${params.toString()}`);
+  // };
 
   return (
     <div className="flex">
       {index != 0 && <span className="flex whitespace-pre"> - </span>}
-      <button
+      <Link
         className="text-nowrap hover:text-[--midnight-green] hover:underline"
-        onClick={() => handleClick(name)}
+        href={`https://tu-amigo-fi.vercel.app/materias?search=${name
+          .normalize('NFD')
+          .replace(/[\u0300-\u036f]/g, '')}`}
+        // onClick={() => handleClick(name)}
       >
         {name}
-      </button>
+      </Link>
     </div>
   );
 }
