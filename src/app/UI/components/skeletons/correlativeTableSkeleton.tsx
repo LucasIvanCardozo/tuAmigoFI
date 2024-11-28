2a790aca8256efda101f15ead9408df4c8170f7b
Lucas Cardozo <lucasivancardozo27@gmail.com>
2024-11-28 15:28:22 -0300

    mejora en altura de lista de correlativas

--- src/app/UI/components/skeletons/correlativeTableSkeleton.tsx
+++ src/app/UI/components/skeletons/correlativeTableSkeleton.tsx
@@ -3,7 +3,7 @@ import { CgArrowRightO } from 'react-icons/cg';
 export default async function CorrelativeTableSkeleton() {
   return (
     <div className="pl-2 my-1">
-      <div className="flex h-6 gap-1 text-sm text-[--black-olive] sm:h-auto">
+      <div className="flex min-h-6 gap-1 text-sm text-[--black-olive] sm:h-auto">
         <div className="flex items-center h-full justify-between gap-1 self-start w-19">
           <p>Necesitas</p>
           <CgArrowRightO className="self-center text-[--midnight-green]" />
