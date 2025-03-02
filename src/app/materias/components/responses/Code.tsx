5197eae900468931dd00980422d855dceea59c7f
Lucas Cardozo <lucasivancardozo27@gmail.com>
2025-03-02 01:21:00 -0300

    solucion al error de highligh

--- src/app/materias/components/responses/Code.tsx
+++ src/app/materias/components/responses/Code.tsx
@@ -1,25 +1,11 @@
-'use client';
-import React, { useEffect, useRef } from 'react';
-import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css';
-
 interface Params {
   code: string;
 }
 
 export const Code = ({ code }: Params) => {
-  const codeRef = useRef<HTMLModElement>(null);
-
-  useEffect(() => {
-    if (codeRef.current) {
-      const resultado = hljs.highlightAuto(code);
-      codeRef.current.innerHTML = resultado.value;
-    }
-  }, [code]);
-
   return (
     <pre>
-      <code ref={codeRef}>{code}</code>
+      <code>{code}</code>
     </pre>
   );
 };
