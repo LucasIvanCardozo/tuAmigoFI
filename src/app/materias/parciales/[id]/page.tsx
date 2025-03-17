b27437ce0eca202230b17ae6bb265fd46cd6fd53
Lucas Cardozo <lucasivancardozo27@gmail.com>
2025-03-17 17:33:12 -0300

    Actualizar page.tsx

--- src/app/materias/parciales/[id]/page.tsx
+++ src/app/materias/parciales/[id]/page.tsx
@@ -17,6 +17,10 @@ async function Content({ params }: Params) {
 }
 
 export default async function Practica({ params }: Params) {
+const id_materia = Number(params.id);
+  const course = await fetchCourse(id_materia);
+  const moduleList = await fetchMidtermsWithAllData(id_materia);
+  let modules = makeModules({ moduleList: moduleList, type: 'midterms' });
   return (
 <MainModule modules={modules} course={course} typeModule="Practica" />;
     /*<Suspense fallback={<MainSkeleton />}>
