a7ef79518975fea0b39fc618e3e653b72c681307
Lucas Cardozo <lucasivancardozo27@gmail.com>
2025-03-17 13:02:10 -0300

    se prueba el uso de suspense en las paracticas

--- src/app/materias/parciales/[id]/page.tsx
+++ src/app/materias/parciales/[id]/page.tsx
@@ -1,12 +1,24 @@
 import { fetchCourse, fetchMidtermsWithAllData } from '@/app/lib/data';
 import { MainModule } from '../../components/mainModule';
 import { makeModules } from '../../utils/makeModules';
+import { Suspense } from 'react';
 
-export default async function Practica({ params }: { params: { id: string } }) {
+interface Params {
+  params: { id: string };
+}
+
+async function Content({ params }: Params) {
   const id_materia = Number(params.id);
   const course = await fetchCourse(id_materia);
   const moduleList = await fetchMidtermsWithAllData(id_materia);
   let modules = makeModules({ moduleList: moduleList, type: 'midterms' });
-
   return <MainModule modules={modules} course={course} typeModule="Practica" />;
+}
+
+export default async function Practica({ params }: Params) {
+  return (
+    <Suspense fallback={<div>Cargando</div>}>
+      <Content params={params} />
+    </Suspense>
+  );
 }
--- src/app/materias/practica/[id]/page.tsx
+++ src/app/materias/practica/[id]/page.tsx
@@ -1,12 +1,25 @@
 import { fetchCourse, fetchTpsWithAllData } from '@/app/lib/data';
 import { MainModule } from '../../components/mainModule';
 import { makeModules } from '../../utils/makeModules';
+import { Suspense } from 'react';
 
-export default async function Practica({ params }: { params: { id: string } }) {
+interface Params {
+  params: { id: string };
+}
+
+async function Content({ params }: Params) {
   const id_materia = Number(params.id);
   const course = await fetchCourse(id_materia);
   const moduleList = await fetchTpsWithAllData(id_materia);
   let modules = makeModules({ moduleList: moduleList, type: 'tps' });
 
   return <MainModule modules={modules} course={course} typeModule="TP" />;
+}
+
+export default async function Practica({ params }: Params) {
+  return (
+    <Suspense fallback={<div>Cargando</div>}>
+      <Content params={params} />
+    </Suspense>
+  );
 }
